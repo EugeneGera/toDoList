5fb21f86424d8ecffe1618342a042a24a2f0a092
Eugene Gerasimov <eugene.gerasimov@ottofeller.com>
2023-07-14 11:10:29 +0300

    add disabled to button

--- src/common/Button/index.tsx
+++ src/common/Button/index.tsx
@@ -8,9 +8,11 @@ export const Button = function Button(props: {
   onClick?: VoidFunction;
   type?: "button" | "submit" | "reset";
   text?: string;
+  disabled?: boolean;
 }) {
   return (
     <button
+      disabled={props.disabled}
       className={classNames(props.className, {
         "grid grid-flow-col justify-self-end gap-x-2 rounded-md p-1 hover:bg-gray-200 dark:hover:bg-fuchsia-500 dark:bg-fuchsia-600":
           props.text,
@@ -26,7 +28,7 @@ export const Button = function Button(props: {
         width={25}
         height={25}
         className={classNames("rounded-md", {
-          "hover:bg-gray-200 dark:bg-fuchsia-600 dark:hover:bg-fuchsia-500": !props.text,
+          "hover:bg-gray-200 dark:bg-fuchsia-600 dark:hover:bg-fuchsia-500":!props.text,
         })}
       />
     </button>
--- src/common/FilterButton/index.tsx
+++ src/common/FilterButton/index.tsx
@@ -3,15 +3,17 @@ import { Button } from "../Button";
 export const CompletedTasks = function CompletedTasks(props: {
   filterTask: VoidFunction;
   showFilterTask: boolean;
+  className?: string;
+  disabled: boolean
 }) {
   return (
-      <Button
-        src="filter.svg"
-        alt="filter"
-        onClick={props.filterTask}
-        text={`Показать ${
-          props.showFilterTask ? "все" : "невыполненные"
-        } задачи`}
-      />
+    <Button
+      disabled={props.disabled}
+      src="filter.svg"
+      alt="filter"
+      onClick={props.filterTask}
+      text={`Показать ${props.showFilterTask ? "все" : "невыполненные"} задачи`}
+      className={props.className}
+    />
   );
 };
