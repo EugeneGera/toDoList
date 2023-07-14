7753b33b34723a888d46854864c308e50ff3de7f
Eugene Gerasimov <eugene.gerasimov@ottofeller.com>
2023-07-14 11:11:28 +0300

    fix hydration error

--- src/common/SwitchTheme/index.tsx
+++ src/common/SwitchTheme/index.tsx
@@ -3,14 +3,13 @@ import { Button } from "../Button";
 
 export const SwitchTheme = function SwitchTheme() {
   const { theme, setTheme } = useTheme();
-  const currentTheme = theme === 'dark'
 
   return (
-      <Button
-        src="light.svg"
-        alt="light"
-        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-        text={`Поменять на ${currentTheme ? "тёмную" : "светлую"} тему`}
-      />
+    <Button
+      src="light.svg"
+      alt="light"
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      text="Поменять тему"
+    />
   );
 };
